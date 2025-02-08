1c8e8e0b7f9f2c458355adee70f2d856677c7bfd
C0deFreak <grgurinluka@gmail.com>
2025-02-08 23:30:05 +0100

    Merge cleanup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{svelte,ts,js}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
